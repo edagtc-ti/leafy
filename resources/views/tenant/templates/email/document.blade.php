72fbaca53cf92bed697f83fcedf4ad59d20449b4
Eduardo Castro <ed.agtc@gmail.com>
2022-04-18 02:18:39 -0500

    FIRMA PARA EL ENVÍO DE CORREOS

--- resources/views/tenant/templates/email/document.blade.php
+++ resources/views/tenant/templates/email/document.blade.php
@@ -30,5 +30,42 @@
     <li>Nro. de comprobante: {{ $document->series.'-'.$document->number }}</li>
     <li>Total: {{ $document->total }}</li>
 </ul>
+
+
+<table border=0>
+    <tr>
+        <td>
+       
+        @if($company->logo)
+            <td width="20%">
+                <div class="company_logo_box">
+                    <img src="data:{{mime_content_type(public_path("storage/uploads/logos/{$company->logo}"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/{$company->logo}")))}}" alt="{{$company->name}}" class="company_logo" style="max-width: 150px;">
+                </div>
+            </td>
+        @else
+            <td width="20%">
+                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
+            </td>
+        @endif
+
+        </td>
+        <td>
+            <h1>{{ $document->user->name }}<h1>
+            <h3>Asesor de ventas</h3>
+            {{ $document->user->email }}
+            <br>
+            <h4>{{ $company->name }}</h4>
+            {{ 'RUC '.$company->number }}<br>
+            Of. {{ $document-> establishment -> address }} Telf.: {{ $document->establishment->telephone }}<br>
+            {{ $document-> establishment -> trade_address}}<br>
+            Website : {{ $document-> establishment -> web_address}}
+        </td>
+
+    </tr>
+    
+</table>
+
+
+
 </body>
 </html>
--- resources/views/tenant/templates/email/quotation.blade.php
+++ resources/views/tenant/templates/email/quotation.blade.php
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -18,10 +20,49 @@
 
     {{ $company->name }}
   
-    , informamos que su cotización ha sido emitida exitosamente.</p>
+    , informamos que su cotización ha sido emitida exitosamente..</p>
 
 <ul>
 
 </ul>
+
+<table border=0>
+    <tr>
+          
+        @if($quotation-> establishment -> web_address=='www.tsl.pe')
+            <td width="20%">
+                <div class="company_logo_box">
+                    <img src="data:{{mime_content_type(public_path("storage/uploads/logos/logo_20602143016.png"))}};base64, {{base64_encode(file_get_contents(public_path("storage/uploads/logos/logo_20602143016.png")))}}" alt="TSL OPERADOR LOGISTICO S.A.C" class="company_logo" style="max-width: 150px;">
+                </div>
+            </td>
+        @else
+            <td width="20%">
+                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
+            </td>
+        @endif
+                      
+        <td> 
+            <h1>{{ $quotation->user->name }}<h1>
+            <h3>Asesor de ventas</h3>
+            {{ $quotation->user->email }}
+            <br>
+            @php
+            if($quotation-> establishment -> web_address=='www.tsl.pe'){
+                @endphp
+                <h4>TSL OPERADOR LOGISTICO S.A.C</h4>
+                RUC 20602143016<br>
+                @php
+            }
+            @endphp
+            Of. {{ $quotation-> establishment -> address }} Telf.: {{ $quotation->establishment->telephone }}<br>
+            {{ $quotation-> establishment -> trade_address}}<br>
+            Website : {{ $quotation-> establishment -> web_address}}
+            
+        </td>
+      
+    </tr>
+    
+</table>
+
 </body>
 </html>
